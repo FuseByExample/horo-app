072eab41043a5e892c8dcb05be524a71d5b02c33
Jakub Korab <jakub.korab@gmail.com>
2012-06-26 17:06:46 +0100

    Added RssConsumerRouteBuilder and test

---
+++ horo-rss-reader/src/main/java/net/jakubkorab/horo/rssReader/RssConsumerRouteBuilder.java
@@ -0,0 +1,56 @@
+package net.jakubkorab.horo.rssReader;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang.Validate;
+
+/**
+ * Template {@link RouteBuilder} for routes that consume RSS feeds and persist these to the daabase. 
+ * @author jkorab
+ */
+public class RssConsumerRouteBuilder extends RouteBuilder {
+	
+	private String sourceName;
+	private String sourceUri;
+	private String targetUri;
+
+	public void setSourceName(String sourceName) {
+		this.sourceName = sourceName;
+	}
+
+	public void setSourceUri(String sourceUri) {
+		this.sourceUri = sourceUri;
+	}
+
+	public void setTargetUri(String targetUri) {
+		this.targetUri = targetUri;
+	}
+
+	@PostConstruct
+	public void checkMandatoryProperties() {
+		Validate.notEmpty(sourceName);
+		Validate.notEmpty(sourceUri);
+		Validate.notEmpty(targetUri);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.apache.camel.builder.RouteBuilder#configure()
+	 */
+	@Override
+	public void configure() throws Exception {
+		from(sourceUri).id("rssConsumer-" + sourceName)
+			.split(xpath("/rss/channel/item"))
+			.setHeader("pubdate", xpath("/item/pubdate"))
+			.transform(xpath("/item/description"))
+			.transform(bean(StringEscapeUtils.class, "unescapeHtml"))
+			.unmarshal().tidyMarkup()
+			// get the first paragraph
+			.transform(xpath("//p[1]/text()").stringResult())
+			.to("log:items")
+			// save to db
+			.to(targetUri);
+	}
+
+}
---
+++ horo-rss-reader/src/test/java/net/jakubkorab/horo/rssReader/RssConsumerRouteBuilderTest.java
@@ -0,0 +1,65 @@
+package net.jakubkorab.horo.rssReader;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.apache.commons.lang.SystemUtils;
+import org.apache.commons.lang.Validate;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RssConsumerRouteBuilderTest extends CamelTestSupport {
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	protected RouteBuilder createRouteBuilder() throws Exception {
+		RssConsumerRouteBuilder builder = new RssConsumerRouteBuilder();
+		builder.setSourceName("test");
+		builder.setSourceUri("direct:in");
+		builder.setTargetUri("mock:out");
+		return builder;
+	}
+
+	@Test
+	public void test() throws InterruptedException, IOException {
+		MockEndpoint mock = getMockEndpoint("mock:out");
+		mock.setExpectedMessageCount(12);
+
+		String body = loadFileAsString("src/test/resources/astrology.com/2012-06-25.xml");
+		template.sendBody("direct:in", body);
+
+		mock.setResultWaitTime(1000);
+		mock.assertIsSatisfied();
+		log.info(mock.getReceivedExchanges().get(0).getIn().getBody()
+				.toString());
+	}
+
+	private String loadFileAsString(String fileName) throws IOException {
+		Validate.notEmpty(fileName);
+		FileInputStream in = null;
+		try {
+			in = new FileInputStream(fileName);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException("Could not find " + fileName, e);
+		}
+		BufferedReader bufferedReader = new BufferedReader(
+				new InputStreamReader(in));
+		StringBuilder out = new StringBuilder();
+		while (true) {
+			String line = bufferedReader.readLine();
+			if (line == null) {
+				break;
+			} else {
+				out.append(line + SystemUtils.LINE_SEPARATOR);
+			}
+		}
+		return out.toString();
+	}
+}
