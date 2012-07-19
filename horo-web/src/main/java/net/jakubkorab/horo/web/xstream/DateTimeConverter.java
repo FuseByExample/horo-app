5db741659060d7f09b1009e2b30e8e6910cf5dda
Jakub Korab <jakub.korab@gmail.com>
2012-07-19 18:44:11 +0100

    Added xstream for a simple view tech.

---
+++ horo-web/src/main/java/net/jakubkorab/horo/web/xstream/DateTimeConverter.java
@@ -0,0 +1,38 @@
+package net.jakubkorab.horo.web.xstream;
+
+import com.thoughtworks.xstream.converters.Converter;
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
+
+public class DateTimeConverter implements Converter {
+
+    @Override
+    public boolean canConvert(Class clazz) {
+        return clazz.isAssignableFrom(DateTime.class);
+    }
+
+    @Override
+    public void marshal(Object value, HierarchicalStreamWriter writer,
+                        MarshallingContext context) {
+        DateTime dateTime = (DateTime) value;
+        DateTimeFormatter formatter = ISODateTimeFormat.basicDateTime().withZoneUTC();
+        writer.startNode("dateTime");
+        writer.setValue(formatter.print(dateTime));
+        writer.endNode();
+    }
+
+    @Override
+    public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+        reader.moveDown();
+        DateTimeFormatter formatter = ISODateTimeFormat.basicDateTime().withZoneUTC();
+        DateTime dateTime = formatter.parseDateTime(reader.getValue());
+        reader.moveUp();
+        return dateTime;
+    }
+
+}
